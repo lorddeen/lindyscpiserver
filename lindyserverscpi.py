88575e556f6bf67bf0e2242a04b8841c3e8d03f8
Tomáš Lindner <lindner.prac@gmail.com>
2025-12-24 14:46:10 +0100

    Add sys import and define DEVICE constant
    added sys package gor CLI control and DEVICE variable.

--- lindyserverscpi.py
+++ lindyserverscpi.py
@@ -3,10 +3,14 @@
 import scv
 import socket
 import threading
+import sys
 
-def __init__(self)
+
+def __init__(self) # defining basic parameters
   HOST = 192.168.69.69
   PORT = 5085
+  DEVICE = "MULTIMETER"
+  
 
 def main(self)
   
